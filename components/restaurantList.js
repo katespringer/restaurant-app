dd11c2b202eef6903ca63812cdc0e2db8c5a4541
katespringer <93007058+katespringer@users.noreply.github.com>
2022-06-15 00:56:02 -0400

    Update restaurantList.js

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -15,11 +15,8 @@ import {
   Row,
   Col} from "reactstrap";
 
-function RestaurantList(props){
-  const[restaurantID, setRestaurantID] = useState(0)
-  const {cart } = useContext(AppContext);
-  const [state, setState] = useState(cart)
-  const GET_RESTAURANTS = gql`
+
+const GET_RESTAURANTS = gql`
     query {
       restaurants {
         data {
@@ -39,60 +36,50 @@ function RestaurantList(props){
       }
     }
   `;
-  const { loading, error, data } = useQuery(GET_RESTAURANTS)
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>ERROR</p>;
-  if (!data) return <p>Not found</p>;
-  console.log(`Query Data: ${data.restaurants}`)
+  const RestaurantList = (props) => {
+    const [restaurantID, setRestaurantID] = useState(0);
+    const { loading, error, data } = useQuery(GET_RESTAURANTS);
+    if(loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+    if (!data) return <p>Not Found</p>
+    const renderDishes = (restaurantID) => { 
+        return <Dishes restId={restaurantID} />;
+    };
+    const renderRestaurants = restaurants => {
+        return (
+            <>
+                {restaurants.map(restaurant => (
+                    <Col xs='6' sm='4' key={restaurant.id}>
+                        <Card style={{margin: '0.5rem 0.5rem 20px 0.5rem'}}>
+                            <CardImg top={true} style={{height: 200}} src={`http://localhost:1337${restaurant.attributes.image.data.attributes.url}`} />
+                            <CardBody>
+                                <CardText>{restaurant.attributes.description}</CardText>
+                            </CardBody>
+                            <div className='card-footer'>
+                                <Button color='info' onClick={() => setRestaurantID(restaurant.id)}>{restaurant.attributes.name}</Button>
+                            </div>
+                        </Card>
+                    </Col>
+                ))}
+            </>
+        )
+    };
 
-
-if (data.restaurants?.data?.length) {
-    const searchQuery = data.restaurants.data.filter((query) =>
-      query.attributes.name.toLowerCase().includes(props.search)
-    );
-let restId = searchQuery[0].id
- 
-// definet renderer for Dishes
-  const renderDishes = (restaurantID) => {
-    return (<Dishes restId={restaurantID}> </Dishes>)
-  };
-if(searchQuery.length > 0){
-  const restList = searchQuery.map(({ id, attributes }) => (
-    <Col xs="6" sm="4" key={id}>
-      <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-        <CardImg
-          top={true}
-          style={{ height: 200 }}
-           src={`http://localhost:1337${attributes.image.data.attributes.url}`}
-        />
-        <CardBody>
-          <CardText>{attributes.description}</CardText>
-        </CardBody>
-        <div className="card-footer">
-
-        <Button color="info" onClick={()=> setRestaurantID(id)}>{attributes.name}</Button>
-
-        </div>
-      </Card>
-    </Col>
-  ))}
-
-  return(
-
-    <Container>
-    <Row xs='3'>
-      {restList}
-    </Row>
-  
-    <Row xs='3'>
-    {renderDishes(restaurantID)}
-    </Row>
- 
-    </Container>
- 
-  )
-} else {
-  return <h1> No Restaurants Found</h1>
-}
-}
-export default RestaurantList
+    if (data) {
+        const restaurants = props.query === '' ? data.restaurants.data : data.restaurants.data.filter(restaurant => restaurant.attributes.name.toLowerCase().indexOf(props.query.toLowerCase()) !== -1);
+        return (
+            <Container>
+                <Row xs='3'>
+                    {renderRestaurants(restaurants)}
+                </Row>
+                <Row xs='3'>
+                    {renderDishes(restaurantID)}
+                </Row>
+            </Container>
+        );
+    }
+    else {
+        return <h1>No Restaurants Found</h1>
+    }
+};
+export default RestaurantList;
