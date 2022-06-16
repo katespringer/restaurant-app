78317da36d3924dfa2303b218445f0eeedeaa817
katespringer <93007058+katespringer@users.noreply.github.com>
2022-06-15 21:15:45 -0400

    Update context.js

--- components/context.js
+++ components/context.js
@@ -1,16 +1,15 @@
-/* /context/AppContext.js */
-
+//context/AppContext.js
 import React from "react";
 // create auth context with default value
 
-// set backup default for isAuthenticated if none is provided in Provider
-const AppContext = React.createContext(
-    {isAuthenticated:true, 
-        cart: {items:[], 
-        total:0},
-        addItem:()=>{},
-        removeItem:()=>{},
-        user:false, 
-        setUser:()=>{}
-    });
+// set backup default for isAuthenticated
+//if none is provided in Provider
+const AppContext = React.createContext({
+  isAuthenticated: false,
+  cart: { items: [], total: 0 },
+  addItem: () => {},
+  removeItem: () => {},
+  user: null,
+  setUser: () => {},
+});
 export default AppContext;
